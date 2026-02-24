2c01603c8aef7c908c6f31a7f3e9ce09bbe0238a
pragyasharva-dev <pragyasharva.official@gmail.com>
2026-02-24 16:29:13 +0530

    New feature added for updating priority of tasks

---
+++ commands/update.py
@@ -0,0 +1,32 @@
+from storage.json_store import load_tasks, save_tasks
+
+def update_task_priority(task_no : int, desired_pos : int):
+    """Updates the priority of tasks"""
+    tasks = load_tasks() # Loads the tasks from json
+
+    if not tasks:   # Checks for empty task list
+        print("no task to update")
+        return
+
+    index = task_no - 1 # Adjusting the indices for operation 
+    pos = desired_pos - 1
+
+    if index < 0 or index >= len(tasks):  # Input validation
+        print("Invalid task number")
+        return
+
+    if pos < 0 or pos >= len(tasks):        # Input validation
+        print("Invalid destination position")
+        return
+    
+    
+    temp = tasks[index] # Storing the desired task temporarily since it will be popped later
+    tasks.pop(index)    # Removing the task 
+    tasks.insert(pos, temp) # Inserting the task
+    
+
+    save_tasks(tasks)  # Saving the updated changes in json
+
+    print(f"Moved {temp} from {task_no} to {desired_pos}")
+
+
