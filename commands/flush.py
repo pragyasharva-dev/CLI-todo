83eee9fd3f1f0db401661b16395a39372b2d1849
pragyasharva-dev <pragyasharva.official@gmail.com>
2026-02-26 18:05:17 +0530

    feat: introduce flush command to clear all tasks
    Implemented flush_task_list functionality to clear out the entire to-do list. Replaces the current tasks configuration inside tasks.json with an empty JSON structure when executed.

---
+++ commands/flush.py
@@ -0,0 +1,10 @@
+from storage.json_store import TASK_FILE, save_tasks
+import json
+
+def flush_task_list():
+    """Clear out the task list"""
+    task = {'tasks':[]}                       # Simply replace the whole json with a fresh one
+    with open(TASK_FILE, "w") as f:
+        json.dump(task, f, indent=4)
+
+    print("Todo list cleared!")
