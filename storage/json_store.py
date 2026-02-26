86887a9e8c46749b7730c0374d41fbb852be0474
pragyasharva-dev <pragyasharva.official@gmail.com>
2026-02-26 09:55:11 +0530

    refactor: add cache functionality and parameterize file paths Description:
    Refactored ensure_storage, load_tasks, and save_tasks to accept a dynamic filepath argument instead of hardcoding TASK_FILE.
    Added a try-except block to ensure_storage to handle JSONDecodeErrors which ensures safety against corrupted files.
    Introduced a CACHE_FILE path and a MAX_LEN constant.
    Added new helper functions (save_cache_json, ensure_len, and update_cache) to store and manage the historical state of tasks for undo mechanisms.

--- storage/json_store.py
+++ storage/json_store.py
@@ -3,29 +3,70 @@ import json
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent   # Setting up the parent directory
-TASK_FILE = BASE_DIR / "tasks.json"                 # Setting up the json file's path
+TASK_FILE = BASE_DIR / "tasks.json"                 # Setting up the task storage json file's path
+CACHE_FILE = BASE_DIR / "cache.json"                # Setting up the cache memory json file's path
+MAX_LEN = 5
 
 
-def ensure_storage():
+def ensure_storage(filepath):
     """Ensure tasks.json exists with valid structure."""
-    if not TASK_FILE.exists():          # Creates a json file if one doesnt exist
-        with open(TASK_FILE, "w") as f:
+    if not filepath.exists():          # Creates a json file if one doesnt exist
+        with open(filepath, "w") as f:
             json.dump({"tasks":[]}, f)  # Creates an empty task list as well
 
+    try:
+        with open(filepath, "r") as f:
+            json.load(f)
+    except json.JSONDecodeError:
+        with open(filepath, "w") as f:
+            json.dump({"tasks":[]}, f)
 
-def load_tasks():
+
+def load_tasks(filepath):
     """Load the contents of tasks.json"""
-    ensure_storage()                   # Checks for json file
-    with open(TASK_FILE, "r") as f:    # Loads the files
+    ensure_storage(filepath)                   # Checks for json file
+    with open(filepath, "r") as f:    # Loads the files
         data = json.load(f)
 
     return data["tasks"]    # returns the task list
 
 
-def save_tasks(tasks):
+def save_tasks(tasks, filepath):
     """Writes the tasks in tasks.json"""
-    ensure_storage()
-    with open(TASK_FILE, "w") as f:     # Opens the json file in write mode and dumps the changes in the json
+    ensure_storage(filepath)
+    with open(filepath, "w") as f:     # Opens the json file in write mode and dumps the changes in the json
         data = {"tasks":tasks}
         json.dump(data, f, indent=4)
+
+
+def save_cache_json(task):
+    with open(CACHE_FILE, "w") as f:
+        data = {"tasks" : task}
+        json.dump(data, f, indent=4)
+
+
+
+def ensure_len(filepath, maxlen:int):  # this is for the cache file mainly
+    x = load_tasks(filepath)
+    if len(x) > maxlen:
+        new_x = x[-maxlen:]
+        save_tasks(new_x, filepath)
+
+def update_cache():
+    storage = load_tasks(TASK_FILE)
+    #print(f"[DEBUG]: {storage}")
+    cache = load_tasks(CACHE_FILE)
+    #print(f"[DEBUG]: {cache}")
+
+    cache.append(storage)
+    #print(f"[DEBUG]: {cache}")
+
+    '''with open(CACHE_FILE, "w") as f:
+        data = {"tasks" : cache}
+        json.dump(data, f, indent=4)'''
+    save_cache_json(cache)
+
+
+
+
     
