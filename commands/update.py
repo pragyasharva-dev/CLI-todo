22bedbee82ae6ae463c5e05579fe44305f875e40
pragyasharva-dev <pragyasharva.official@gmail.com>
2026-02-26 10:01:28 +0530

    refactor: update load and save task calls with file paths Description:
    Modified load_tasks and save_tasks references to pass TASK_FILE corresponding to the latest storage module update.

--- commands/update.py
+++ commands/update.py
@@ -1,8 +1,8 @@
-from storage.json_store import load_tasks, save_tasks
+from storage.json_store import load_tasks, save_tasks, TASK_FILE
 
 def update_task_priority(task_no : int, desired_pos : int):
     """Updates the priority of tasks"""
-    tasks = load_tasks() # Loads the tasks from json
+    tasks = load_tasks(TASK_FILE) # Loads the tasks from json
 
     if not tasks:   # Checks for empty task list
         print("no task to update")
@@ -25,7 +25,7 @@ def update_task_priority(task_no : int, desired_pos : int):
     tasks.insert(pos, temp) # Inserting the task
     
 
-    save_tasks(tasks)  # Saving the updated changes in json
+    save_tasks(tasks, TASK_FILE)  # Saving the updated changes in json
 
     print(f"Moved {temp} from {task_no} to {desired_pos}")
 
