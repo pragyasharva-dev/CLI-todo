0e585bd24110d877939f5fe57d4ed9311fbd39cf
pragyasharva-dev <pragyasharva.official@gmail.com>
2026-03-02 11:21:31 +0530

    feat(commands): integrate Task object in add operations
    Updated add_task to instantiate and store a Task object from models.classes rather than a raw string.

--- commands/add.py
+++ commands/add.py
@@ -1,7 +1,9 @@
 from storage.json_store import load_tasks, save_tasks, TASK_FILE
+from models.classes import Task
 
 
-def add_task(task:str):
+def add_task(command:str):
+    task = Task(command)  # creates Task object
     tasks = load_tasks(TASK_FILE) # Checks and loads the contents of json file
     tasks.append(task)   # Adds the new task to the loaded json
     save_tasks(tasks, TASK_FILE)    # overwrites the updated task to the previous json
