086708c54120f4d06f8ddca962e89e4ef297cd1d
pragyasharva-dev <pragyasharva.official@gmail.com>
2026-03-05 12:42:40 +0530

    feat(models): add completion tracking to Task model
    Introduced the `completed` attribute to the `Task` object model and implemented a [toggle_completion](cci:1://file:///c:/coactive/CLI-todo/GUI/app.py:101:0-110:88) method. Enhanced the [toggle_priority](cci:1://file:///c:/coactive/CLI-todo/GUI/app.py:90:0-99:86) method with terminal print statements to provide immediate feedback on state changes.

--- models/classes.py
+++ models/classes.py
@@ -2,11 +2,12 @@ from datetime import datetime
 #from zoneinfo import ZoneInfo
 
 class Task:
-    def __init__(self, name: str, priority = False, created_at=None): 
+    def __init__(self, name: str, priority = False, completed=False, created_at=None,): 
         self.name = name          # name of the task
         self.priority = priority  # priority of the task, True : high, False : low
+        self.completed = completed
         self.created_at = created_at or datetime.now()  # Task creation time
-
+        
 
     def to_dict(self):
         """Converts the Task object to dictionary for storing in json"""  
@@ -32,6 +33,18 @@ class Task:
     def toggle_priority(self):
         """Toggles the priority"""
         self.priority = not self.priority
+        if self.priority == True:
+            print(f"{self.name} priority set to high")
+        else:
+            print(f"{self.name} priority set to low")
+
+    def toggle_completion(self):
+        '''Toggles state of completion'''
+        self.completed = not self.completed
+        if self.completed == True:
+            print(f"{self.name} marked as completed")
+        else:
+            print(f"{self.name} marked as uncompleted")
 
     
 
