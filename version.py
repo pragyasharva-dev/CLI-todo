67614844728bc5937ce97ec1bc73a3d7d657f675
pragyasharva-dev <pragyasharva.official@gmail.com>
2026-03-09 16:16:18 +0530

    test version for testing app assets

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 import requests
 from packaging import version
 
-CURRENT_VERSION = "2.0.2"
+CURRENT_VERSION = "2.0.3"
 latest = "https://api.github.com/repos/pragyasharva-dev/CLI-todo/releases/latest"
 
 def check_for_updates():
